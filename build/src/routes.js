a2b136664919b52d274e27e7442e948153086e77
JorgeLNJunior <jorgelnjunior@gmail.com>
2020-11-24 16:43:13 -0300

    fix: retirar arquivos desnecessários

--- build/src/routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const router = express_1.Router();
-router.get('/', (req, res) => {
-    return res.json({ msg: 'hello world' });
-});
-exports.default = router;
