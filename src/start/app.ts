34575485d84c3e768338a7683b815bbe477371c5
JorgeLNJunior <jorgelnjunior@gmail.com>
2020-11-25 16:47:35 -0300

    feat: adicionar o helmet

--- src/start/app.ts
+++ src/start/app.ts
@@ -2,13 +2,15 @@ import 'dotenv/config';
 
 import cors from 'cors';
 import express, { Request, Response } from 'express';
+import helmet from 'helmet';
 
 import router from '../routes';
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
+app.use(cors());
+app.use(helmet());
 app.use(router);
 
 app.use((req: Request, res: Response) => {
