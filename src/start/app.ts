17268da9ad1fee1ee74d0bc09ddf85457cb70993
JorgeLNJunior <jorgelnjunior@gmail.com>
2020-12-23 15:42:54 -0300

    feat: adicionar o swagger

--- src/start/app.ts
+++ src/start/app.ts
@@ -5,9 +5,14 @@ import express, { Request, Response } from 'express';
 import hateLimit from 'express-rate-limit';
 import helmet from 'helmet';
 import path from 'path';
+import { resolve } from 'path';
+import swaggerUi from 'swagger-ui-express';
+import YML from 'yamljs';
 
 import { logger } from '../config/logger';
 import router from '../routes';
+
+const swaggerDoc = YML.load(resolve('src/config/swagger.yml'));
 
 const app = express();
 
@@ -18,6 +23,7 @@ app.use(helmet());
 app.use(logger);
 app.use(hateLimit({ max: process.env.NODE_ENV === 'test' ? 200 : 30 }));
 app.use(router);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.use((req: Request, res: Response) => {
   return res.status(404).json({ error: 'route not found' });
