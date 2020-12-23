cabfafddd6ef86b192b2ba005d752ead68a72cd4
JorgeLNJunior <jorgelnjunior@gmail.com>
2020-12-23 16:26:29 -0300

    feat: personalizar a mensagem de inicialização do servidor

--- src/start/server.ts
+++ src/start/server.ts
@@ -1,7 +1,21 @@
+import chalk from 'chalk';
+
 import app from './app';
 
 const port = parseInt(process.env.PORT as string) || 3000;
 
+const hours = new Date().getHours();
+const minutes = new Date().getMinutes();
+const seconds = new Date().getSeconds();
+const fullHour = `${hours}:${minutes}:${seconds}`;
+
+const startMessage =
+  '[' +
+  chalk.green('SERVER') +
+  '] ' +
+  chalk.gray.bold(fullHour) +
+  chalk.green(` Listening at port ${port}`);
+
 app.listen(port, () => {
-  console.log(`listening on port ${port}`);
+  console.log(startMessage);
 });
