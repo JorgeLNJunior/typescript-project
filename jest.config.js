26e2385fc56d29e8ddb86cb382b92f22e5d167d5
JorgeLNJunior <jorgelnjunior@gmail.com>
2020-12-26 18:43:10 -0300

    test: retirar o diretorio config do coverage

--- jest.config.js
+++ jest.config.js
@@ -20,7 +20,11 @@ module.exports = {
   // collectCoverage: false,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/start/**'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/start/**',
+    '!<rootDir>/src/config/**',
+  ],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: '__tests__/coverage',
