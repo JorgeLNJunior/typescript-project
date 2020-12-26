febc5f370d095e8693d5825b7c17b9ffbbc76448
JorgeLNJunior <jorgelnjunior@gmail.com>
2020-12-26 18:36:23 -0300

    fix(ci): corrigir os testes no travis

--- src/config/database.ts
+++ src/config/database.ts
@@ -6,7 +6,7 @@ export default {
   host: process.env.DB_HOST || 'localhost',
   port: process.env.DB_PORT || 3306,
   username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || 'root',
+  password: process.env.DB_PASS || '',
   database: process.env.DB_NAME || 'test',
   synchronize: false,
   logging: false,
